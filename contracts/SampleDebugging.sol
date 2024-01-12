b51b3b4b43a2a4d307e69a08cfefe62165576211
codexNature <olusolajaiyeola@gmail.com>
2024-01-12 04:15:32 +0100

    complete ABI array

---
+++ contracts/SampleDebugging.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract MyContract {
+
+    uint public myUint = 123;
+
+    function setMyUint(uint newUint) public {
+        myUint = newUint;
+    }
+
+}
