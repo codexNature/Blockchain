550b4ad12c8cdf3fce0b9064c666f4d50fab8354
codexNature <olusolajaiyeola@gmail.com>
2024-01-24 04:06:56 +0100

    tokensale complete with token coffee created to buy coffee

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
@@ -14,20 +15,35 @@ contract CoffeeToken is ERC20, AccessControl {
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    function increaseAllowance(address spender, uint256 addedValue) public  virtual returns (bool) {
+        _approve( _msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero" );
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+        return true;
+    }
+
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+        _mint(to, amount * 10 ** decimals());
     }
 
     function buyOneCoffee() public {
-        _burn(_msgSender(), 1);
+        _burn(_msgSender(), 1 * 10 ** decimals());
         emit coffeePurchased(_msgSender(), _msgSender());
     }
 
     function buyOneCoffeeFrom(address account) public {
-        _spendAllowance(account, _msgSender(), 1);
-        _burn(account, 1);
+        _spendAllowance(account, _msgSender(), 1 * 10 ** decimals());
+        _burn(account, 1 * 10 ** decimals());
         emit coffeePurchased(_msgSender(), account);
     }
 }
 
-//all the functions called are from openzeppelin in github
